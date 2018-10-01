7d878c84260d5d97dd19c6c2003db1e841407621
zrondos <zrondos@github.com>
2018-10-01 15:43:20 -0400

    Add student functionality

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,13 @@ class StudentsController < ApplicationController
     end
 
     def create
-        @student=Student.create(params[:student])
+        @student=Student.create(first_name: params[:student][:first_name],
+        last_name: params[:student][:last_name], 
+        age: params[:student][:age] ,
+        education: params[:student][:education],
+        cohort_id: params[:student][:cohort_id]
+        )
+        redirect_to '/students'
     end
 
 end
