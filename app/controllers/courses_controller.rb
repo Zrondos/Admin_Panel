3eaf6d31fba637535e7170d3ce3a6d4efa262d0d
zrondos <zrondos@github.com>
2018-10-11 11:27:16 -0400

    Heroku configurations

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,7 @@
 class CoursesController < ApplicationController
 
     def index
-        @courses=Course.alljhgl.order('created_at ASC')
+        @courses=Course.all.order('created_at ASC')
     end
 
     def new
