b6d7256676238743b3c00ad67c3de8c8cf421dd3
zrondos <zrondos@github.com>
2018-11-12 11:28:07 -0500

    Styling changes for font, field input, bullet point image

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,53 +1,40 @@
-Course.create(name: 'Pokemon 101',
-            hours: 10
+Course.create(name: 'Vault',
+            hours: 100
             )
-Course.create(name: 'Pokemon 201',
-             hours: 20)
+Course.create(name: 'Uneven Bars',
+             hours: 200)
 
-Cohort.create(name: 'Team Rocket',
+Cohort.create(name: 'Rio 2016',
         start_date: Time.now,
         end_date: Time.now,
         course_id: 1)
 
-Cohort.create(name: 'Team Aqua',
+Cohort.create(name: 'London 2012',
         start_date: Time.now,
         end_date: Time.now,
         course_id: 1)
 
-Cohort.create(name: 'Team Magma',
-        start_date: Time.now,
-        end_date: Time.now,
-        course_id: 1)
-
-Instructor.create(first_name: 'Professor',
-                last_name:'Oak',
+Instructor.create(first_name: 'Marta',
+                last_name:'Karolyi',
                 age: 50,
                 salary: 100,
                 education: 'None',
                 cohort_id: 1)
 
-Instructor.create(first_name: 'Professor',
-                last_name: 'Elm',
+Instructor.create(first_name: 'Coach Jordyn',
+                last_name: 'Wieber',
                 age: 50,
                 salary: 100,
                 education: 'None',
                 cohort_id: 2)
 
-Instructor.create(first_name: 'Professor',
-                last_name: 'Birch',
-                age: 50,
-                salary: 100,
-                education: 'None',
-                cohort_id: 3)
-
-
-Student.create(first_name: 'Ash',
-                last_name: 'Ketchum',
+Student.create(first_name: 'Aly',
+                last_name: 'Raisman',
                 age: 20,
                 education: 'None')
 
-Student.create(first_name: 'Brock',
-                last_name: 'Onix',
+Student.create(first_name: 'Kyla',
+                last_name: 'Ross',
                 age: 20,
                 education: 'None')
 
